bfcc15284dddb21d9526823424e72215f02a822c
l1e <l1e@DESKTOP-9K58KRP>
2024-02-27 21:11:08 +0200

    Добавьте файлы проекта.

---
+++ lab4oop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab4oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double>[] arr = new Func<double, double>[3]
+            {
+                (double x) => Math.Sqrt(Math.Abs(x)),
+                (double x) => Math.Pow(x, 3.0),
+                (double x) => x + 3.5
+            };
+            while (true)
+            {
+                try
+                {
+                    string[] arr2 = Console.ReadLine().Trim().Split(' ');
+                    Console.WriteLine(arr[int.Parse(arr2[0])](double.Parse(arr2[1])));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Помилка");
+                }
+            }
+        }
+    }
+}
